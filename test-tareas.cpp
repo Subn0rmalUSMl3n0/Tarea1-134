732f4ad958dad03b8fd68d78f5c940ff5edbf74c
Subn0rmalUSMl3n0 <rruz@usm.cl>
2024-04-09 16:08:56 -0400

    cambios minimos

--- test-tareas.cpp
+++ test-tareas.cpp
@@ -5,23 +5,23 @@
 /*pasar el string tablerito a un arreglo, y arreglar fn Coordenada*/
 using namespace std;
 
-char* LecturaTexto(string& no_piezas, string& tablerito) {
+char* LecturaTexto(int& no_piezas) {
     ifstream text; /* abre el archivo tablero.txt para lectura*/
     text.open("tablero.txt");
     if (!text.is_open()) {
         cerr << "Error al abrir tablero.txt" << endl;
     }
-    getline(text,no_piezas);    /*obtiene la primera linea del .txt y consigue la cantidad de piezas en juego*/
-
+    text >> no_piezas;    /*obtiene la primera linea del .txt y consigue la cantidad de piezas en juego*/
+    string temptablero;
     string temp;
     for (int i=0; i<8; i++) {
         text >> temp; /*se lee el texto de la i-esima fila del .txt y se guarda en temp*/
-        tablerito= tablerito + temp; /*tablero aloja la fila actual de temp antes de que se pierda, con un espacio entre medias pues solo puede trabajarse en una linea 1x64, no una matriz 8x8*/
+        temptablero= temptablero + temp; /*tablero aloja la fila actual de temp antes de que se pierda, con un espacio entre medias pues solo puede trabajarse en una linea 1x64, no una matriz 8x8*/
     };
-    char* CharPanchito= new char[tablerito.length()+1]; /*Crea un char temporal para pasar el tablero de string a char*/
+    char* CharPanchito= new char[temptablero.length()+1]; /*Crea un char array dinamico para pasar el tablero de string a char*/
     int j = 0;
-    while(j < tablerito.length()){
-        CharPanchito[j] = tablerito[j]; /*para cada simbolo del tablerito, se pasa a charpanchito*/
+    while(j < temptablero.length()){
+        CharPanchito[j] = temptablero[j]; /*para cada simbolo del tablerito, se pasa a charpanchito*/
         j++;
     };
     CharPanchito[j] = '\0'; /*se agrega el caracter nulo a charpanchito, para que no de errores*/
@@ -29,19 +29,17 @@ char* LecturaTexto(string& no_piezas, string& tablerito) {
     return CharPanchito;
 };
 
-void SimboloCoordenada(string tablerito, string no_piezas) {
-    (string) tablerito;
-    char piezas[stoi(no_piezas)+1];
+void SimboloCoordenada(char* tablerito, int no_piezas) {
+    char piezas[no_piezas+1];
     int i= 0;
     string punto = ".";
     char ji[] = "pene";
-    return ;
+    cout << no_piezas << tablerito << endl; /*printea la cantidad de piezas en juego*/
 };
 int main() {
-    string no_piezas; /*Numero de piezas en juego*/
-    string tablerito; /*Tablero traspuesto del .txt a un string*/
-    char* Charpanchito;
-    Charpanchito = LecturaTexto(no_piezas,tablerito); /* inicia la funcion y devuelve los valores actualizados*/
+    int no_piezas; /*Numero de piezas en juego*/
+    char* tablerito; /*tablero del .txt*/
+    tablerito = LecturaTexto(no_piezas); /* inicia la funcion y devuelve los valores actualizados*/
 
     struct Pieza {
         char simbolo; /*tipo de pieza y su caracter*/
@@ -52,11 +50,10 @@ int main() {
         Pieza* piezas_tablero; /*lista de piezas en tablero*/
     };
     Pieza pieza; /*para modificar los parámetros de struct pieza*/
-    SimboloCoordenada(tablerito,no_piezas);
+    SimboloCoordenada(tablerito, no_piezas);
     
     Tablero tablero; /*para modificar los parámetros de struct tablero*/
-    tablero.cantidad_piezas = stoi(no_piezas); /*traspasa de string a int*/
-    cout << tablero.cantidad_piezas << Charpanchito << endl; /*printea la cantidad de piezas en juego*/
+    tablero.cantidad_piezas = no_piezas; /*traspasa de string a int*/
     return 0;
 };
 
