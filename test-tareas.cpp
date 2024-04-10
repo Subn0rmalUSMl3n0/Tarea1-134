baa5506127e2eb012e3f9ccd9c4a32aa64a1f6b2
Subn0rmalUSMl3n0 <rruz@usm.cl>
2024-04-10 00:15:51 -0400

    Cambios mayores, Structs correctamente configurados

--- test-tareas.cpp
+++ test-tareas.cpp
@@ -2,58 +2,53 @@
 #include <fstream>
 #include <cstring>
 #include <string>
-/*pasar el string tablerito a un arreglo, y arreglar fn Coordenada*/
 using namespace std;
 
-char* LecturaTexto(int& no_piezas) {
-    ifstream text; /* abre el archivo tablero.txt para lectura*/
-    text.open("tablero.txt");
+struct Pieza {
+        char simbolo; /*tipo de pieza y su caracter*/
+        int x,y; /* posicion en el tablero [0,7]*[0,7]*/
+};
+struct Tablero {
+        int cantidad_piezas; /*numero de piezas en juego*/
+        Pieza* piezas_tablero; /*lista de piezas en tablero*/
+};
+Tablero tablerito;
+
+Pieza* LecturaTexto(int &no_piezas) {
+    ifstream text; 
+    text.open("tablero.txt"); /* abre el archivo tablero.txt para lectura*/
     if (!text.is_open()) {
         cerr << "Error al abrir tablero.txt" << endl;
     }
-    text >> no_piezas;    /*obtiene la primera linea del .txt y consigue la cantidad de piezas en juego*/
-    string temptablero;
-    string temp;
-    for (int i=0; i<8; i++) {
-        text >> temp; /*se lee el texto de la i-esima fila del .txt y se guarda en temp*/
-        temptablero= temptablero + temp; /*tablero aloja la fila actual de temp antes de que se pierda, con un espacio entre medias pues solo puede trabajarse en una linea 1x64, no una matriz 8x8*/
+    text >> no_piezas; /*obtiene la primera linea del .txt y consigue la cantidad de piezas en juego*/
+    tablerito.cantidad_piezas = no_piezas; /* se actualiza la cantidad de piezas del struct Tablero*/
+    Pieza* pieza = new Pieza[no_piezas]; /*para modificar los parámetros de struct pieza*/
+    char temp[2]; /* se crea un string temporal para alojar cada caracter del .txt y el caracter nulo*/
+    int k = 0;
+    for (int i = 0; i < 8; ++i) { /*mide cada fila de las 8 del tablero*/
+        for (int j = 0; j < 8; ++j) { /*mide cada columna de las 8 del tablero*/
+            text >> temp[0]; /*El caracter del texto se va al string temporal*/
+            int p = strcmp(temp, "."); /*Si el struct temporal es un punto, retorna 0, si no, retorna otro valor...*/
+            if(p != 0) { /*...haciendo que este if solo se cumpla para caracteres distintos del punto*/
+                pieza[k].simbolo = temp[0]; 
+                pieza[k].x = i;
+                pieza[k].y = j; /*la pieza numero k del struct pieza, recibe a la vez su simbolo y sus coordenadas*/
+                cout << pieza[k].simbolo << " " << pieza[k].x << " , " << pieza[k].y << endl;
+                ++k; /* la proxima vez que haya una pieza, se registrara en un nuevo arreglo del struct*/
+            };
+        };    
     };
-    char* CharPanchito= new char[temptablero.length()+1]; /*Crea un char array dinamico para pasar el tablero de string a char*/
-    int j = 0;
-    while(j < temptablero.length()){
-        CharPanchito[j] = temptablero[j]; /*para cada simbolo del tablerito, se pasa a charpanchito*/
-        j++;
-    };
-    CharPanchito[j] = '\0'; /*se agrega el caracter nulo a charpanchito, para que no de errores*/
-    text.close(); /*se cierra el .txt*/
-    return CharPanchito;
+    return pieza;
 };
 
-void SimboloCoordenada(char* tablerito, int no_piezas) {
-    char piezas[no_piezas+1];
-    int i= 0;
-    string punto = ".";
-    char ji[] = "pene";
-    cout << no_piezas << tablerito << endl; /*printea la cantidad de piezas en juego*/
-};
 int main() {
     int no_piezas; /*Numero de piezas en juego*/
-    char* tablerito; /*tablero del .txt*/
-    tablerito = LecturaTexto(no_piezas); /* inicia la funcion y devuelve los valores actualizados*/
+    tablerito.piezas_tablero = LecturaTexto(no_piezas); /*invoca a la funcion, y devuelve los arreglos de simbolos y coordenadas de las piezas*/
 
-    struct Pieza {
-        char simbolo; /*tipo de pieza y su caracter*/
-        int x,y; /* posicion en el tablero [0,7]*[0,7]*/
-    };
-    struct Tablero {
-        int cantidad_piezas; /*numero de piezas en juego*/
-        Pieza* piezas_tablero; /*lista de piezas en tablero*/
-    };
-    Pieza pieza; /*para modificar los parámetros de struct pieza*/
-    SimboloCoordenada(tablerito, no_piezas);
-    
-    Tablero tablero; /*para modificar los parámetros de struct tablero*/
-    tablero.cantidad_piezas = no_piezas; /*traspasa de string a int*/
+    cout<< tablerito.piezas_tablero[0].simbolo << endl;
+    cout << tablerito.piezas_tablero[0].x << endl;
+    cout << tablerito.piezas_tablero[0].y << endl;
+    cout << tablerito.cantidad_piezas << endl;
     return 0;
 };
 
